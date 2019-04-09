61d933557933911321a559f4f2dd025aff2c96f3
Pavitra Golchha <pavitragolchha@gmail.com>
2019-04-09 18:00:15 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+const $url = document.querySelector('input')
+
+const urlParams = new URLSearchParams(window.location.search)
+
+if (urlParams.has('url')) {
+    $url.value = urlParams.get('url')
+    update($url.value)
+}
+
+$url.addEventListener('keydown', e => {
+    if (e.key === 'Enter') {
+        update(e.target.value)
+    }
+})
+
+function update(value) {
+    const $thead = document.querySelector('table thead'),
+        $tbody = document.querySelector('table tbody')
+
+    if ($thead.computedStyleMap().get('display').value === 'none') {
+        $thead.attributeStyleMap.set('display', 'table-header-group')
+    }
+
+    $tbody.innerHTML = ''
+
+    const keys = [
+        'hostname',
+        'href',
+        'origin',
+        'pathname',
+        'username',
+        'password',
+        'protocol',
+        'port',
+        'search',
+        'hash'
+    ]
+
+    const url = new URL(value)
+
+    keys.filter(k => url[k])
+        .forEach(k => insertTableRow($tbody, k, url[k]))
+}
+
+function insertTableRow(table, key, value) {
+    const row = table.insertRow(-1),
+        cell1 = row.insertCell(0),
+        cell2 = row.insertCell(1)
+
+    cell1.textContent = key
+    cell2.textContent = value
+}
